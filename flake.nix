a53f4b523f3e25ea4d56c64cf0992c66568f537f
Diego A. Estrada Rivera <daestrada@pm.me>
2024-10-18 15:21:06 -0400

    ci: Add Python support

--- flake.nix
+++ flake.nix
@@ -1,39 +1,47 @@
 {
-  description = "A Nix-flake-based C/C++ development environment";
+    description = "A Nix-flake-based C/C++ development environment";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
+    outputs = { self, nixpkgs }:
+        let
+        supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+            pkgs = import nixpkgs { inherit system; };
+            });
     in
     {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell.override {
-          # Override stdenv in order to change compiler:
-          # stdenv = pkgs.clangStdenv;
-        }
-        {
-          packages = with pkgs; [
-            clang-tools
-            clang
-            cmake
-            codespell
-            conan
-            cppcheck
-            doxygen
-            gtest
-            lcov
-            vcpkg
-            vcpkg-tool
-            ccache
-            llvmPackages_12.openmp
-            dash
-          ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
-        };
-      });
+        devShells = forEachSupportedSystem ({ pkgs }: {
+                default = pkgs.mkShell {
+                    venvDir = "venv";
+                    packages = with pkgs; [
+                        clang-tools
+                        clang
+                        cmake
+                        codespell
+                        conan
+                        cppcheck
+                        doxygen
+                        gtest
+                        lcov
+                        vcpkg
+                        vcpkg-tool
+                        ccache
+                        llvmPackages_12.openmp
+                        dash
+                        # Python dependencies
+                        sage
+                        python311
+                        python312Packages.pip
+                        python312Packages.venvShellHook
+                        python312Packages.cython
+                        python312Packages.numpy
+                        python312Packages.matplotlib
+                        python312Packages.jupyter-core
+                        python312Packages.jupyterlab
+                        python312Packages.nbformat
+                    ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
+                };
+        });
     };
 }
