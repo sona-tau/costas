d276aae50c35f06fbd00eadaf39c6e3524eca46b
Sona Tau Estrada Rivera <sona@stau.space>
2026-04-03 14:04:37 -0400

    feat: simplified search utilities

--- src/julia/search.jl
+++ src/julia/search.jl
@@ -1,9 +1,36 @@
-include("search3.jl")
+using Transducers
+import Serialization
 
-for i in 15:26
-	c = tsearchbt(i)
-	println("Found $(length(c)) $(i)x$(i) arrays")
-	cls = classes(c)
-	println("Found $(length(cls)) $(i)x$(i) classes")
-	savevar("classes_$(i)x$(i).dat", cls)
+include("../../notebooks/shared/header.jl")
+include("../../CostasArrays.jl/src/io.jl")
+include("../../CostasArrays.jl/src/search.jl")
+include("../../CostasArrays.jl/src/symmetry.jl")
+include("../../CostasArrays.jl/src/verify.jl")
+
+function savetofile(fpath::String, c::Vector{Vector{Integer}})
+	text = totxt(c)
+	open(fpath, "w") do file
+		write(file, text)
+	end
 end
+
+function orbitsofn(n::Int)::Vector{Vector{Int}}
+	orbits = tsearchbt(n) |> classes
+end
+
+function genall(c::AbstractVector{AbstractVector{<:Integer}})::Vector{Vector{Int}}
+	foldxt(vcat, c .|> unique ∘ d4orbit) |> sort
+end
+
+function generate(n::Int)
+	println("generating ", n, " by ", n, " Costas arrays")
+	orbits::Vector{Vector{Integer}} = tsearchbt(n) |> classes
+	println("generated ", length(orbits), " orbits")
+	all::Vector{Vector{Integer}} = foldxt(vcat, orbits .|> unique ∘ d4orbit) |> sort
+	println("generated ", length(all), " Costas arrays")
+	savetofile("../../data/orbits_$(n)x$(n).txt", orbits)
+	savetofile("../../data/costas_$(n)x$(n).txt", all)
+	nothing
+end
+
+generate(10)
